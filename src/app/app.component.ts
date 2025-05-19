4be37c90a00b1dfd2ee185549ba6752d626a5fe6
nissrine-elf <elfijaoui.nissrine@gamil.com>
2025-05-19 11:56:58 +0100

    examenFront2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Component, OnInit  } from '@angular/core';
 import { RouterOutlet ,RouterModule } from '@angular/router';
-import { NavbarComponent } from './nav/nav.component';
+import { NavComponent } from './nav/nav.component';
+import { ClientComponent } from './client/client.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavComponent,RouterModule,ClientComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/services/client.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import{Observable} from "rxjs";
+import {Client } from '../model/client.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+   constructor(private http: HttpClient) { }
+   public getClients() :Observable<Array<Client>>{
+     return this.http.get<Array<Client>>("http://localhost:8082/clients")
+     }
+
+     public saveClient(clients :Client):Observable<Client>{
+       return this.http.post<Client>("http://localhost:8082/clients",clients)
+       }
+      public deleteCustomer(id :number){
+           return this.http.delete("http://localhost:8082/clients/" +id)
+           }
+}
