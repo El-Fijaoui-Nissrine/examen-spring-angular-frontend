1b793f99295577b20617ccf0415a2615e7ea66c9
nissrine-elf <elfijaoui.nissrine@gamil.com>
2025-05-19 11:48:00 +0100

    examenFront

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Component, OnInit  } from '@angular/core';
+import { RouterOutlet ,RouterModule } from '@angular/router';
+import { NavbarComponent } from './nav/nav.component';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+                               constructor( ){}
+                                     ngOnInit():void{
+
+                                  }
   title = 'examen-frontend-web';
 }
